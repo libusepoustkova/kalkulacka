d66c1b3b82c1d1aaf9938c97bd73e49d8e3f1648
Libuse Poustkova <libuse.poustkova@seznam.cz>
2017-12-09 13:20:04 +0100

    upravena cesta a udelan pyz

--- kalkulacka/logic.py
+++ kalkulacka/logic.py
@@ -1,10 +1,12 @@
 from PyQt5 import QtWidgets, uic
+import pkg_resources
 def main():
     #vytvoreni aplikace a okna
     app=QtWidgets.QApplication([])
     window=QtWidgets.QDialog()
     #nacteni ovladatek
-    with open('kalkulacka/kalkulacka.ui',encoding='UTF-8') as f: #f je soubor, with samo zajisti zavreni souboru)
+    #with open('kalkulacka/kalkulacka.ui',encoding='UTF-8') as f: #f je soubor, with samo zajisti zavreni souboru,  ) prvni verze s primou cestou
+    with pkg_resources.resource_stream('kalkulacka','kalkulacka.ui') as f: #druha verze pro zabaleni, bere jmeno modulu a souboru kterz je na stejne urovni, vedle nej
         uic.loadUi(f,window)
 
     sb_operand1=window.findChild(QtWidgets.QDoubleSpinBox,'sb_operand1')
